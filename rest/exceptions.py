f0d8e5f248ada052349499ebd8153e18ab8f5ddd
Alexey K <avkhatskevich@gmail.com>
2016-05-23 19:40:04 +0300

    added error printing ot log

--- rest/exceptions.py
+++ rest/exceptions.py
@@ -1,6 +1,11 @@
+import logging
+
 from rest_framework.exceptions import APIException
+
+logr_requests = logging.getLogger('requests')
 
 class MY_REST_EXCEPTION(APIException):
     def __init__(self, status, detail=""):
         self.status_code = status
         self.detail = detail
+        logr_requests.info(str(status) + " " + str(detail))
